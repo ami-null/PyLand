10220ead72bfa4de64cfa970f6d127956d319213
Md. Aminul Islam Shazid <70378126+ami-null@users.noreply.github.com>
2026-06-06 02:09:02 +0600

    get rid of unnecessary backslash after %~dp0

--- dependencies/_config.bat
+++ dependencies/_config.bat
@@ -4,9 +4,9 @@
 :: %~dp0 here refers to this file's own directory (dependencies\helper_scripts).
 
 set "ROOT_DIR=%~dp0"
-set "UV_DIR=%ROOT_DIR%\uv"
+set "UV_DIR=%ROOT_DIR%uv"
 set "UV_EXE=%UV_DIR%\uv.exe"
-set "PYTHON_DIR=%ROOT_DIR%\python"
+set "PYTHON_DIR=%ROOT_DIR%python"
 set "PYTHON_EXE=%PYTHON_DIR%\python.exe"
 set "SCRIPTS_DIR=%PYTHON_DIR%\Scripts"
 set "HELPER_SCRIPTS_DIR=%ROOT_DIR%\helper_scripts"
