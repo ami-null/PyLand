2fc6c86f0e980cfd58c6b2908d0ad5b9d1b886c4
Md. Aminul Islam Shazid <70378126+ami-null@users.noreply.github.com>
2026-06-09 18:55:23 +0600

    Update pip.bat

--- dependencies/helper_scripts/pip.bat
+++ dependencies/helper_scripts/pip.bat
@@ -1,5 +1,6 @@
 @echo off
-uv pip %*
+:: "%UV_EXE%" is set by the _config.bat script
+"%UV_EXE%" pip %*
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 if /i "%1" == "install" (
     if not defined SKIP_PORTABLIZE "%PYTHON_EXE%" "%HELPER_SCRIPTS_DIR%\make-portable-exe.py"
